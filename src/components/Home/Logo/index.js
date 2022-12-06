23a13c4726be9d6528e1e04a629dd803688a4611
Yasser Mechrem <yasser.mechrem29@gmail.com>
2022-12-06 19:58:16 +0100

    Update index.js

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,13 +1,12 @@
 import './index.scss'
 import LogoS from '../../../assets/images/logo-s.png'
-import { useEffect, useRef } from 'react'
+import { useRef } from 'react'
 // import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
-import gsap from 'gsap-trial'
+
 
 const Logo = () => {
 
     const bgRef = useRef();
-    const outlineLogoRef = useRef();
     const solidLogoRef = useRef();
 
     // useEffect(() => {
